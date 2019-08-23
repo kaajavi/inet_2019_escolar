563d55b68b5064f3c32b3fc6e6fc3814f900cc3d
kaajavi <javierguignard@gmail.com>
2019-08-23 09:51:01 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import cherrypy
+from datetime import datetime, timedelta
+from random import randrange
+
+cherrypy.config.update({'server.socket_port': 9090})
+VIENTO=['NORTE','NOR ESTE','ESTE','SUR ESTE', 'SUR','SUR OESTE','OESTE','NOR OESTE','NORTE']
+
+def create_key_value():
+    now = datetime.now()
+    minute = int('{}0'.format(now.minute//10))
+    now = now.replace(minute=minute, second=0, microsecond=0) 
+    context = {}
+    from_time = now - timedelta(hours=24)
+    while now > from_time:
+        context['{}'.format(now)]={'v':randrange(185,245)/10.0,
+                                    'i':randrange(0,300)/10.0,
+                                    'w_s':randrange(150,950)/10.0,
+                                    'w_d':VIENTO[randrange(0,7)]}
+        now = now - timedelta(minutes=10)
+    return context
+
+class Root(object):
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def index(self):
+        return create_key_value()
+
+if __name__ == '__main__':
+   cherrypy.quickstart(Root(), '/')
